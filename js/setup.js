f40b2ac2901fc064e96c209738331dc1cdfdc50d
kalashnikovsb <kalashnikov.sk8@gmail.com>
2020-06-15 00:31:06 +0700

    Выполнил задание 6

--- js/setup.js
+++ js/setup.js
@@ -9,6 +9,16 @@ var setup = document.querySelector('.setup');
 var setupSimilar = document.querySelector('.setup-similar');
 var wizardsList = document.querySelector('.setup-similar-list');
 var wizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var setupOpen = document.querySelector('.setup-open-icon');
+var setupClose = setup.querySelector('.setup-close');
+var userNameInput = setup.querySelector('.setup-user-name');
+var wizardCoat = setup.querySelector('.setup-wizard .wizard-coat');
+var wizardEyes = setup.querySelector('.setup-wizard .wizard-eyes');
+var wizardFireball = setup.querySelector('.setup-fireball-wrap');
+var wizardCoatInput = setup.querySelector('input[name="coat-color"]');
+var wizardEyesInput = setup.querySelector('input[name="eyes-color"]');
+var wizardFireballInput = setup.querySelector('input[name="fireball-color"]');
+var FIREBALLS_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 var getRandomNumber = function (min, max) {
   min = Math.ceil(min);
@@ -56,8 +66,69 @@ var renderWizardsList = function (wizards) {
   wizardsList.appendChild(fragment);
 };
 
+var onSetupEscPress = function (evt) {
+  if (evt.keyCode === 27) {
+    closeSetup();
+  }
+};
+
+var openSetup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onSetupEscPress);
+};
+
+var closeSetup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onSetupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openSetup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 13) {
+    openSetup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closeSetup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 13) {
+    closeSetup();
+  }
+});
+
+userNameInput.addEventListener('focus', function () {
+  document.removeEventListener('keydown', onSetupEscPress);
+});
+
+userNameInput.addEventListener('blur', function () {
+  document.addEventListener('keydown', onSetupEscPress);
+});
+
+wizardCoat.addEventListener('click', function () {
+  var coatColor = getRandomArrayElement(COATS_COLORS);
+  wizardCoat.style.fill = coatColor;
+  wizardCoatInput.value = coatColor;
+});
+
+wizardEyes.addEventListener('click', function () {
+  var eyesColor = getRandomArrayElement(EYES_COLORS);
+  wizardEyes.style.fill = eyesColor;
+  wizardEyesInput.value = eyesColor;
+});
+
+wizardFireball.addEventListener('click', function () {
+  var fireballColor = getRandomArrayElement(FIREBALLS_COLORS);
+  wizardFireball.style.background = fireballColor;
+  wizardFireballInput.value = fireballColor;
+});
+
 // Основной код
-setup.classList.remove('hidden');
 setupSimilar.classList.remove('hidden');
 var wizards = getWizardsArray();
 renderWizardsList(wizards);
